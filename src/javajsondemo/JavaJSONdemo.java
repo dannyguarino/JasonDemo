f1418e0add19a62f14d66c0ac84eedce5f6e5ac3
dannyguarino <guarinodanny@gmail.com>
2015-01-23 17:14:42 -0500

    removed the "println"

--- src/javajsondemo/JavaJSONdemo.java
+++ src/javajsondemo/JavaJSONdemo.java
@@ -19,61 +19,35 @@ public class JavaJSONdemo {
     public static void main(String[] args) {
         
         if (args.length != 2) {
-            System.out.println("Erreur de parametres");
+            System.out.println("Erreur arguments invalide");
             System.exit(0);
         }
+        String inputfile = args[0];
+        String outputfile= args [1];
         
         JSONParser parser = new JSONParser();
         try {
-            Object obj = parser.parse(new FileReader("inputfile.json"));
+            Object obj = parser.parse(new FileReader(inputfile));
             JSONObject jsonObj = (JSONObject) obj;
             
             String client = (String) jsonObj.get("client");
             String typeContrat = (String) jsonObj.get("contrat");
             String mois = (String) jsonObj.get("mois");
-            
-            JSONArray reclamations = (JSONArray) jsonObj.get("reclamations");
-            
-            System.out.println("Client :" + client);
-            System.out.println("Type de contract: " + typeContrat);
-            System.out.println("Mois: " + mois);
-            
-            System.out.println(reclamations.toString());
-            System.out.println("--------");
-            
-            System.out.println(reclamations.get(0).toString());
-            System.out.println(reclamations.get(1).toString());
-            System.out.println(reclamations.get(2).toString());
-            
             DetailClient objClient = new DetailClient(client, mois, typeContrat);
             
-            System.out.println("-------------------");
-            System.out.println("Client :" + objClient.getClientID());
-            System.out.println("Type de contract: " + objClient.getContrat());
-            System.out.println("Mois: " + objClient.getDate());
-            System.out.println("+++++++++++++++++++");
+            JSONArray reclamations = (JSONArray) jsonObj.get("reclamations");
             
             Iterator i = reclamations.iterator();
             
             while (i.hasNext()) {
                 JSONObject recDetail = (JSONObject) i.next();
-                System.out.println("Soin: " + recDetail.get("soin"));
-                System.out.println("Date: " + recDetail.get("date"));
-                System.out.println("Montant" + recDetail.get("montant"));
-                System.out.println("-----------");
                 long soin = (long) recDetail.get("soin");
                 String date = (String) recDetail.get("date");
                 String montant = (String) recDetail.get("montant");
-                System.out.println("" + soin + "," + date +"," + montant);
                 objClient.ajoutReclamations(soin, date, montant);
                 
             }
-            System.out.println("-------------------");
-            System.out.println("Client :" + objClient.getClientID());
-            System.out.println("Type de contract: " + objClient.getContrat());
-            System.out.println("Mois: " + objClient.getDate());
             
-            System.out.println(objClient.toString());
             List<DetailRecl> tableau = objClient.getReclamations();
             
             System.out.println(tableau.toString());
